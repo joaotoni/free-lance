243010fe2d9d87cd8e20e734f1987a1b848b3e99
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-10-18 16:30:38 -0300

    Refactoring the code

--- pages/home.js
+++ pages/home.js
@@ -5,12 +5,10 @@ import Header from "../components/header/Header";
 export default function Home(){
     return(
         <div className="bg-[rgba(43,43,45,1)]">
-            <Header>
+            <Header />
 
-            </Header>
-            <Footer>
-                
-            </Footer>
+            <Footer />
+            
         </div>
     )
 }

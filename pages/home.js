e0bef34742d552946d72c6317f83e3fea43c7b06
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-10-18 12:39:37 -0300

    Starting the project

---
+++ pages/home.js
@@ -0,0 +1,9 @@
+
+
+export default function Home(){
+    return(
+        <div className="bg-blue-800 text-blue-800">
+            <h1 className="bg-blue-800 text-blue-800">Ola Joao</h1>
+        </div>
+    )
+}

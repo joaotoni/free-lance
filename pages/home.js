b077a0da5455664c0362f1a258c8915e17a2e4a3
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-10-19 14:02:20 -0300

    Finishing the page home and putting colors in tailwind config

--- pages/home.js
+++ pages/home.js
@@ -7,56 +7,57 @@ export default function Home(){
         <main className="bg-[rgba(43,43,45,1)]">
             <Header />
             <section className="flex justify-center mt-6 ">
-                <div className="bg-[rgba(25,182,229,0.5)] shadow-md rounded-[40px] text-center m-0 w-[300px] h-[262px]">
-                    <h2 className="mt-[23px] mb-[16px] text-xl font-semibold text-[#FFFFFF] leading-[27px]">Compre sua conta <br></br> com o Alado!</h2>
-                    <div className="flex ">
+                <div className="bg-azul_fundo shadow-md rounded-[40px] text-center m-0 w-[300px] h-[270px] ">
+                    <h2 className="mt-[23px] mb-[16px] text-xl font-semibold font-montserrat text-branco leading-[27px]">Compre sua conta <br></br> com o Alado!</h2>
+                    <div className="flex p-2 ">
                         <div>
                             <Image src="/assets/icons/safe.png" alt="" width={50} height={50}/>
-                            <p className="text-[10px] font-semibold leading-3 text-[#FFFFFF]">Compra 100% safe.</p>  
+                            <p className="text-[10px] font-semibold leading-3 font-montserrat text-branco">Compra 100% safe.</p>  
                         </div>
                         <div>
                             <Image src="/assets/icons/venda.png" alt="" width={50} height={50}/>
-                            <p className="text-[10px] font-semibold leading-3 text-[#FFFFFF]">Mais de 4 mil contas vendidas.</p>  
+                            <p className="text-[10px] font-semibold leading-3 font-montserrat text-branco">Mais de 4 mil contas vendidas.</p>  
                         </div>
                         <div>
                             <Image src="/assets/icons/entrega.png" alt="" width={50} height={50}/>
-                            <p className="text-[10px] font-semibold leading-3 text-[#FFFFFF]">Entrega imediata.</p>  
+                            <p className="text-[10px] font-semibold leading-3 font-montserrat text-branco">Entrega imediata.</p>  
                         </div>
                     </div>
-                    <button className="mt-[36px] bg-[#19B6E5] rounded-[40px] p-2">
-                        <p className="text-[#FFFFFF] text-[15px] leading-[18px] font-semibold">Compre já!</p>
+                    <button className="mt-[36px] bg-azul_letras rounded-[40px] p-2">
+                        <p className="text-branco font-montserrat text-[15px] leading-[18px] font-semibold">Compre já!</p>
                     </button>
                 </div>
             </section>
-            <hr className="bg-[#19B6E5]  border-solid mt-[18px]"></hr>
+            <hr className="bg-azul_letras border-[1px] border-solid border-azul_letras mt-[18px]"></hr>
             <section className="flex justify-center">
                 <div className="text-center">
-                    <h1 className="text-[22px] text-[#19B6E5] leading-[22px] font-bold mb-[19px] mt-[18px]">Feedbacks</h1>
+                    <h1 className="text-[22px] text-azul_letras leading-[22px] font-montserrat font-bold mb-[19px] mt-[18px]">Feedbacks</h1>
                     <Image src="/assets/imgs/retangulo.png" alt="" width={320} height={197}/>
                 </div>               
             </section>
-            <hr className="bg-[#19B6E5]  border-solid mt-[18px]"></hr>
+            <hr className="bg-azul_letras border-[1px] border-solid border-azul_letras mt-[18px]"></hr>
             <section className="flex justify-center">
                 <div className="text-center">
-                    <h1 className="text-[22px] text-[#19B6E5] leading-[22px] font-bold mb-[19px] mt-[18px]">Lançamentos</h1>
-                    <div>
-                        <div className="bg-[rgba(25,182,229,0.5)] w-[320px] h-[428px]">
+                    <h1 className="text-[22px] text-azul_letras font-montserrat leading-[22px] font-bold mb-[19px] mt-[18px]">Lançamentos</h1>
+                    <div className="overflow-x-scroll scroll-smooth flex gap-6 w-full">
+                        <div className="bg-azul_fundo w-[320px] h-[428px] p-4 ">
                             <Image src="/assets/imgs/image.png" alt="" width={288} height={270}/>
-                            <p className=" text-[#FFFFFF] text-[15px] leading-[18px]">Calça angelical, top criminal azul,<br></br> 18 passes, etc.</p>
-                            <div className="flex justify-center">
-                                <p>de: R$ 300,00</p>
-                                <p>por: R$ 200,00</p>
+                            <p className=" text-branco text-[15px] font-montserrat leading-[18px]">Calça angelical, top criminal azul,<br></br> 18 passes, etc.</p>
+                            <div className="flex justify-center pt-2">
+                                <p className="text-[#8E8E93] font-montserrat pr-[12px]">de: R$ 300,00</p>
+                                <p className="text-branco font-montserrat">por: R$ 200,00</p>
                             </div>
-                            <button className="border-[#19B6E5] border px-2 py-1 bg-[#19B6E5] rounded-full text-center">
-                                <Image src="/assets/icons/carrinho.png" alt="" width={18.75} height={18.75}/>
+                            <button className="border-azul_letras border px-2 py-1 bg-azul_letras rounded-full text-center mt-4">
+                                <Image src="/assets/icons/carrinho.png" alt="" width={19} height={19}/>
                             </button>
                         </div>
                     </div>
-                    <button>
-                        <p>Ver todas</p>
+                    <button className=" border border-azul_letras rounded-[40px] p-1 px-4 mt-4">
+                        <p className="text-azul_letras font-montserrat">Ver todas</p>
                     </button>
                 </div>
             </section>
+            <hr className="bg-azul_letras border-[1px] border-solid border-azul_letras mt-[18px] mb-[18px]"></hr>
             <Footer />   
         </main>
     )

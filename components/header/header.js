065042250d735fac437cb9943b134abbc73f5af4
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-10-26 21:21:32 -0300

    Doing menu hamburguer functional

--- components/header/header.js
+++ components/header/header.js
@@ -1,12 +1,16 @@
 import Image from "next/image"
 import Link from "next/link"
+import React, { useState } from "react"
 
 export default function Header(){
+    const [active, setActive] = useState(false)
     return(
         <header className="flex justify-center  items-baseline pt-3 relative">
-            <button className="absolute mr-[300px] mt-[20px]">
-                <Image src="/assets/icons/menu.png" alt="" width={30} height={30}/>
-            </button>
+            <nav>
+                <button onClick={() => setActive(!active)} className="absolute mr-[300px] mt-[20px]">
+                    <Image src="/assets/icons/menu.png" alt="" width={30} height={30}/>
+                </button>
+            </nav> 
             <nav>
                 <Link href="/">
                     <a>
@@ -14,7 +18,17 @@ export default function Header(){
                     </a>
                 </Link>
             </nav>
-            
+            <div className={active ? "fixed w-full h-[100vh] bg-[rgba(43,43,45,1)] translate-x-[50%] ease-in-out" : " fixed w-full h-[100vh] bg-[rgba(43,43,45,1)] translate-x-[100%] ease-in-out"}>
+                    <Link href="/">
+                        <a>Home</a>
+                    </Link>
+                    <Link href="/catalago">
+                        <a>Catalago</a>
+                    </Link>
+                    <Link href="/perfil">
+                        <a>Perfil</a>
+                    </Link> 
+                </div>
         </header>
     )
 }

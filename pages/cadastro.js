02bab5ffb6b9f68c94637f98ac0d74966981816a
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-11-11 13:43:53 -0300

    Doing input for video

--- pages/cadastro.js
+++ pages/cadastro.js
@@ -2,26 +2,26 @@ import Header from "../components/header/Header";
 
 export default function Cadastro(){
     return(
-        <main>
+        <main className="bg-[rgba(43,43,45,1)]">
             <Header />
-            <section className="text-center">
-                <h1>Cadastre aqui sua conta com o Alado!</h1>
-                <div className="flex flex-col p-4">
-                    <label>Email:</label>
+            <section className="text-center p-4">
+                <h1 className="text-azul_letras">Cadastre aqui sua conta com o Alado!</h1>
+                <for className="flex flex-col p-4 text-branco">
+                    <label className="pb-2">Email:</label>
                     <input type="email" />
-                    <label>Nome da conta:</label>
+                    <label className="pb-2">Nome da conta:</label>
                     <input type="text" />
-                    <label>Nome:</label>
+                    <label className="pb-2">Nome:</label>
                     <input type="text" />
-                    <label>Nome das suas Skins:</label>
+                    <label className="pb-2">Nome das suas Skins:</label>
                     <input type="text" />
-                    <label>Rank da sua conta:</label>
+                    <label className="pb-2">Rank da sua conta:</label>
                     <input type="text" />
-                    <label>Level da sua conta</label>
+                    <label className="pb-2">Level da sua conta</label>
                     <input type="text" />
-                    <label>Coloque um video mostrando o inventario da sua conta:</label>
-                    <input type="" />
-                </div>
+                    <label className="pb-2">Coloque um video mostrando o inventario da sua conta:</label>
+                    <input type="file"name="file" formAction="mp4" />
+                </for>
             </section>
         </main>
     )

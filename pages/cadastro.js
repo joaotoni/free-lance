1f8c7ac995e72bd34b1d5cbe4ca702d0d47f3133
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-11-16 09:19:12 -0300

    Add enviar button in cadastro page

--- pages/cadastro.js
+++ pages/cadastro.js
@@ -4,25 +4,26 @@ import { lista_texto, lista_check} from "../objects/list_of_cadastro";
 
 export default function Cadastro(){
     return(
-        <main className="bg-[rgba(43,43,45,1)]">
+        <main className="bg-fundo">
             <Header />
             <section className="text-center p-4">
                 <h1 className="text-azul_letras">Cadastre aqui sua conta com o Alado!</h1>
-                <for className="flex flex-col p-4 text-branco bg-azul_fundo rounded-lg">
+                <for className="flex flex-col p-4 items-center text-branco bg-azul_fundo rounded-lg ">
                     {lista_texto.map((element, index)=> (
-                        <div className="pb-2 flex flex-col" key={element.text +index}>
-                            <label >{element.text}</label>
+                        <div className="p-2 flex flex-col" key={element.text +index}>
+                            <label className="text-branco font-montserrat text-[15px] leading-[18px] font-semibold" >{element.text}</label>
                             <input className="rounded-lg" type="text" />
                         </div>
                     ))}   
                      {lista_check.map((element, index)=> (
-                        <div className="flex justify-evenly" key={element.check +index}>
-                            <label className="">{element.check}</label>
+                        <div className="flex justify-evenly p-2" key={element.check +index}>
+                            <label className="text-branco font-montserrat text-[15px] leading-[18px] font-semibold">{element.check}</label>
                             <input type="checkbox" />
                         </div>
                     ))}   
-                    <label className="pb-2">Coloque um video mostrando o inventario da sua conta:</label>
+                    <label className="text-branco font-montserrat text-[15px] leading-[18px] font-semibold p-2">Coloque um video mostrando o inventario da sua conta:</label>
                     <input type="file"name="file" formAction="mp4" />
+                    <button className="mt-[36px] bg-azul_letras rounded-[40px] p-2 px-4">Enviar</button>
                 </for>   
             </section>
             <hr className="bg-azul_letras border-[1px] border-solid border-azul_letras mt-[10px] mb-[18px]"></hr>

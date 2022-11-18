cd1c3cd71657341583c01af503b08daf8097e7a1
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-11-18 14:09:43 -0300

    Starting catalago pagination

--- pages/catalago.js
+++ pages/catalago.js
@@ -1,8 +1,11 @@
 import Header from "../components/header/header"
 import Footer from "../components/footer/footer"
 import Card from "../components/card/card";
+import { useRouter } from 'next/router'
 
-export default function Catalago(){
+export default function Catalago({numberOfCards, page}){
+    const router = useRouter()
+    const lastPage = Math.ceil(numberOfCards / 3)
     return(
         <main className="bg-fundo">
             <Header />
@@ -10,11 +13,13 @@ export default function Catalago(){
                 <Card />
                </section>
                <section className="flex justify-around items-center mt-[22px] mb-[80px]">
-                    <button className="border border-azul_letras rounded-[40px] p-1 px-4">
+                    <button className="border border-azul_letras rounded-[40px] p-1 px-4" onClick={() => router.push(`/catalago?page=${page - 1}`)}
+                        disabled={page <= 1}>
                         <p className="text-azul_letras">Anterior</p>
                     </button>
                     <p className="text-azul_letras">2/10</p>
-                    <button className="border border-azul_letras rounded-[40px] p-1 px-4">
+                    <button className="border border-azul_letras rounded-[40px] p-1 px-4" onClick={() => router.push(`/catalago?page=${page + 1}`)}
+                        disabled={page >= lastPage}>
                         <p className="text-azul_letras">Próxima</p>
                     </button>
                </section>

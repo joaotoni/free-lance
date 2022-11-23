d5cd8e4530d234f02f9cd3bbe8e39f8d51716060
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-11-23 11:30:34 -0300

    Doing somes logics for pagination

--- components/pagination/pagination.js
+++ components/pagination/pagination.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const MAX_ITEMS = 9;
-const MAX_LEFT = (MAX_ITEMS -1)/2;
+const MAX_LEFT = (MAX_ITEMS -1) /2;
 const Pagination = ({limit, total, offset}) =>{
-
+    const current = offset ? (offset / limit) +1 : 1;
+    const pages = Math.ceil(total / limit);
+    const first = Math.max(current - MAX_LEFT, 1);
 }
